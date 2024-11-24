1eb282b5f88c390e088e798a90c8b8b3d953383d
musamarasli38 <musa.marasli38@gmail.com>
2024-11-24 21:34:08 +0100

    feature/hero update + assets added

--- musa-marasli/src/components/sub/hero-content.tsx
+++ musa-marasli/src/components/sub/hero-content.tsx
@@ -46,8 +46,9 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          Hi, I am Musa Marasli. I&apos;m a Full Stack Software Engineer with
+          experience in Website, Mobile, and Software development. Check out my
+          projects and skills.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
